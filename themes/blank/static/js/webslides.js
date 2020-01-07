00098864576f76d47a569c03ad17c5b948562ca7
ramiro <rejkpp@users.noreply.github.com>
2020-01-07 22:54:29 +0100

    remove = from slideRegex.

--- themes/blank/static/js/webslides.js
+++ themes/blank/static/js/webslides.js
@@ -1627,7 +1627,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var HASH = '#';
-var slideRegex = /#=(\d+)/;
+var slideRegex = /#(\d+)/;
 
 /**
  * Static class with methods to manipulate and extract info from the hash of
